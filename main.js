1aac5a8d33823ea6d6f39e7c9d470ca95e4ba031
Seogo <77395226+ksh6940@users.noreply.github.com>
2024-11-25 09:02:13 +0900

    main.js 업데이트

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const { MongoClient, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 
-const mongoUri = 'mongodb+srv://<username>:<password>@cluster.mongodb.net/myDatabase?retryWrites=true&w=majority';
+const mongoUri = 'mongodb+srv://rkdtjrgh124:AOxDsBMgg8cP0Gih@webtable.g2ha8vc.mongodb.net/Node-API?retryWrites=true&w=majority&appName=WebTable';
 const client = new MongoClient(mongoUri, { readPreference: 'secondaryPreferred' }); // Secondary 노드 우선 읽기
 
 let gridfsBucket;
 
 async function initialize() {
   await client.connect();
-  const db = client.db('myDatabase');
+  const db = client.db('WebTable');
   gridfsBucket = new mongodb.GridFSBucket(db, { bucketName: 'myBucket' });
   console.log('MongoDB 연결 성공');
 }
@@ -31,7 +31,7 @@ app.get('/api/download/:fileId', async (req, res) => {
     res.set('Content-Type', file.contentType);
     res.set('Content-Disposition', `attachment; filename="${file.filename}"`);
 
-    // 파일 스트리밍
+    // 파일 We
     const downloadStream = gridfsBucket.openDownloadStream(fileId);
     downloadStream.pipe(res);
   } catch (error) {
